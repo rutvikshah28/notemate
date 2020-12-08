98aa3aa1b9615cf79508102226c774049776a73a
Rutvik Shah <53801022+rutvikshah28@users.noreply.github.com>
2020-12-08 09:07:42 +0530

    Create firebase.js

---
+++ src/services/firebase.js
@@ -0,0 +1,25 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+
+
+
+import firebase from 'firebase';
+
+const config = {
+  apiKey: "",
+  authDomain: "",
+  databaseURL: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: "",
+  measurementId: ""
+};
+
+if (!firebase.apps.length) {
+  firebase.initializeApp(config);
+}
+export const auth = firebase.auth;
+export const db = firebase.database();
+export const storage = firebase.storage();
+
+//I have removed my firebase config details from the github code for security purposes.
